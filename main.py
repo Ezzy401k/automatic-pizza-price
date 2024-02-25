d8904b61154025a18bb934eff558206e05cafcbd
Esrael Mekdem <esraelmekdem@gmail.com>
2024-02-25 16:19:47 +0300

    Update main.py
    added comments and shrinked the code

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 print("welcome to the automatic pizza order machine!!")
+# input fields.
 size = input("what size of pizza do you want? L, M, S\n")
 pepperoni = input("Do you want pepperoni in it? y or n.\n")
 cheese = input("Do you want extra cheese? y or n.\n")
+# initiate price variable.
 price = 0
-
+# If L is inputted do this.
 if size == "L":
+    # change price to 25.
     price = 25
     if pepperoni == "y":
+        # add 3 to the price if pepperoni is y.
         price += 3
-    if cheese == "y":
-        price += 1
-    print(f"Your final bill is: {price}$")
+# If M is inputted do this.   
 elif size == "M":
     price = 20
     if pepperoni == "y":
         price += 3
-    if cheese == "y":
-        price += 1
-    print(f'Your final bill is: {price}$')
+# If S is Inputted do this.
 elif size == "S":
     price += 15
     if pepperoni == "y":
         price += 2
-    if cheese == "y":
+# If cheeses are needed add 1 to the price
+if cheese == "y":
         price += 1
-    print(f"Your final bill is: {price}$")
+print(f'Your final bill is: {price}$')
